fcab82e93261278404621aae3ec6758e389bc26e
batuhanbaybas <b.baybas@gmail.com>
2023-04-29 13:42:46 +0300

    creare category operations

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+  @Get('all')
+  async getAllCategories() {
+    await this.categoryService.getAllCategories();
+  }
+
+  @Get(':id')
+  async getCategoryById(@Param('id') id: string) {
+    await this.categoryService.getCategoryById(id);
+  }
+
+  @Delete(':id')
+    async deleteCategoryById(@Param('id') id: string) {
+    await this.categoryService.deleteCategoryById(id);
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllCategories() {
+    const category = await this.prisma.category.findMany();
+
+    return {
+      data: category,
+    };
+  }
+
+  async getCategoryById(id: string) {
+    return await this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+
+  async deleteCategoryById(id: string) {
+    return await this.prisma.category.delete({
+      where: { id },
+    });
+  }
+}
