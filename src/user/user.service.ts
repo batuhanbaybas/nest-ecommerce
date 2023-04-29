73b6e0d5bb82181ead135a2889bcd1a8d4dd4540
batuhanbaybas <b.baybas@gmail.com>
2023-04-29 13:35:57 +0300

    create user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUSerDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async updateUser(id: string, data: UpdateUSerDto) {
+    const user = await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+    delete data.password;
+    return user;
+  }
+
+  async deleteUser(id: string) {
+    return await this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
