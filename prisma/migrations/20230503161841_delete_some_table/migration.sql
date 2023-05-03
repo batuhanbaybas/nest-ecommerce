f3f3f3c89a8a6ca585fda3ecdc50bd1ae2327c01
batuhanbaybas <b.baybas@gmail.com>
2023-05-03 19:20:47 +0300

    delete some service and new migration

---
+++ prisma/migrations/20230503161841_delete_some_table/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Adress` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `OrderItem` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Role` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Shop` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ShopProduct` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Adress";
+
+-- DropTable
+DROP TABLE "Order";
+
+-- DropTable
+DROP TABLE "OrderItem";
+
+-- DropTable
+DROP TABLE "Role";
+
+-- DropTable
+DROP TABLE "Shop";
+
+-- DropTable
+DROP TABLE "ShopProduct";
